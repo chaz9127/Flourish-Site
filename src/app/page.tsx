c020c7a1a8af9c06ea0f85a1d92500a42521fbab
CJ <chasanid@gmail.com>
2026-02-28 15:30:50 -0500

    Finish PixelCard, adding cn() for tailwind and tailwind prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,34 @@
-import GrassFooter from "@/components/GrassFooter";
-import PixelCard from "@/components/PixelCard";
-import Image from "next/image";
+import GrassFooter from '@/components/GrassFooter'
+import PixelCard from '@/components/PixelCard'
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="font-pixelfy">
-      <div className="flex flex-col gap-12 items-center">
-        <div className="flourish-title flex mt-10 items-end gap-4">
-          <h1 className="text-9xl text-[#297C18] leading-24">Flourish</h1>
-          <Image
-            src={'/icons/plants/apple_tree_5.png'}
-            alt={'Apple Tree'}
-            width={52}
-            height={66}
-            className="h-[66px]"
-          />
-        </div>
-        <PixelCard />
-        <button className="pixel-btn max-w-32">
-          Download
-        </button>
-        <div className="relative w-full max-w-[600px]">
-          <Image
-            src='/images/flourish_example.gif'
-            alt='Example of Flourish Video'
-            fill  
-            className="relative"
-          />
-        </div>
-      </div>
-      <GrassFooter />
-    </main>
-  );
+    return (
+        <main className="font-pixelfy">
+            <div className="flex flex-col items-center gap-12">
+                <div className="flourish-title mt-10 flex items-end gap-4">
+                    <h1 className="text-9xl leading-24 text-[#297C18]">
+                        Flourish
+                    </h1>
+                    <Image
+                        src={'/icons/plants/apple_tree_5.png'}
+                        alt={'Apple Tree'}
+                        width={52}
+                        height={66}
+                        className="h-[66px]"
+                    />
+                </div>
+                <PixelCard className="cursor-pointer">Download</PixelCard>
+                <div className="relative w-full max-w-[600px]">
+                    <Image
+                        src="/images/flourish_example.gif"
+                        alt="Example of Flourish Video"
+                        fill
+                        className="relative"
+                    />
+                </div>
+            </div>
+            <GrassFooter />
+        </main>
+    )
 }
--- src/components/PixelCard.tsx
+++ src/components/PixelCard.tsx
@@ -1,3 +1,22 @@
-export default function PixelCard() {
-    return <div className="border-3 border-[var(--green-primary)]">Test</div>
+import { type ReactNode } from 'react'
+
+import { cn } from '@/lib/utils'
+
+export default function PixelCard({
+    className,
+    children,
+}: {
+    className?: string
+    children: ReactNode
+}) {
+    return (
+        <div
+            className={cn(
+                'border-3 border-[var(--green-primary)] bg-[var(--green-highlight)] px-1 py-0.5 shadow-[4px_4px_0px_var(--green-primary)] transition-all duration-200 hover:-translate-x-0.5 hover:-translate-y-0.5 hover:shadow-[6px_6px_0px_var(--green-primary)]',
+                className
+            )}
+        >
+            {children}
+        </div>
+    )
 }
