442216b9721ec806c46d9671a4071a015cdc3a86
CJ <chasanid@gmail.com>
2026-02-28 15:01:20 -0500

    beginning to include new pixel cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import GrassFooter from "@/components/GrassFooter";
+import PixelCard from "@/components/PixelCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="font-pixelfy">
-      <div className="flex flex-col gap-16 items-center">
+      <div className="flex flex-col gap-12 items-center">
         <div className="flourish-title flex mt-10 items-end gap-4">
           <h1 className="text-9xl text-[#297C18] leading-24">Flourish</h1>
           <Image
@@ -15,9 +16,18 @@ export default function Home() {
             className="h-[66px]"
           />
         </div>
+        <PixelCard />
         <button className="pixel-btn max-w-32">
           Download
         </button>
+        <div className="relative w-full max-w-[600px]">
+          <Image
+            src='/images/flourish_example.gif'
+            alt='Example of Flourish Video'
+            fill  
+            className="relative"
+          />
+        </div>
       </div>
       <GrassFooter />
     </main>
---
+++ src/components/PixelCard.tsx
@@ -0,0 +1,8 @@
+
+export default function PixelCard() {
+    return (
+        <div className="border-3 border-[var(--green-primary)]">
+            Test
+        </div>
+    )
+}
