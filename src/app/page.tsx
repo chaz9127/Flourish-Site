cbf8dde4154f53cb3ac9c1ae654f50b0b56149d5
CJ <chasanid@gmail.com>
2026-02-27 21:30:31 -0500

    adding empty download button and adding a green background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flourish-title flex mt-10 w-full justify-center items-end gap-4">
-        <h1 className="text-9xl font-pixelfy text-[#297C18] leading-24">Flourish</h1>
-        <Image
-          src={'/icons/plants/apple_tree_5.png'}
-          alt={'Apple Tree'}
-          width={52}
-          height={66}
-          className="h-[66px]"
-        />
+    <main className="font-pixelfy">
+      <div className="flex flex-col gap-16 items-center">
+        <div className="flourish-title flex mt-10 items-end gap-4">
+          <h1 className="text-9xl text-[#297C18] leading-24">Flourish</h1>
+          <Image
+            src={'/icons/plants/apple_tree_5.png'}
+            alt={'Apple Tree'}
+            width={52}
+            height={66}
+            className="h-[66px]"
+          />
+        </div>
+        <button className="pixel-btn max-w-32">
+          Download
+        </button>
       </div>
       <GrassFooter />
     </main>
